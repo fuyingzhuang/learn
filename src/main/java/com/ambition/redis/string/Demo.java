9cc309a42871371cf3c4cfb00181dbde82338e66
fuyingzhuang <fyz15731966959@163.com>
2023-12-17 21:18:54 +0800

    1. redis string 学习

---
+++ src/main/java/com/ambition/redis/string/Demo.java
@@ -0,0 +1,10 @@
+package com.ambition.redis.string;
+
+/**
+ * @author Ambition
+ * @date 2023/12/17 21:02
+ */
+public class Demo {
+
+
+}
--- src/test/java/com/ambition/LearnApplicationTests.java
+++ src/test/java/com/ambition/LearnApplicationTests.java
@@ -1,13 +1,21 @@
 package com.ambition;
 
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootTest
 class LearnApplicationTests {
 
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
     @Test
-    void contextLoads() {
+    void test() {
+        stringRedisTemplate.opsForValue().set("name", "ambition");
+        System.out.println(stringRedisTemplate.opsForValue().get("name"));
     }
 
 }

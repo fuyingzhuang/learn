856cacd7d2a504b7f4c28df1d14c5fce4b2300c0
fuyingzhuang <fyz15731966959@163.com>
2023-12-18 21:46:04 +0800

    1. redis 常用命令整理

--- src/test/java/com/ambition/LearnApplicationTests.java
+++ src/test/java/com/ambition/LearnApplicationTests.java
@@ -7,6 +7,22 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootTest
 class LearnApplicationTests {
+    /**
+     * keys * 查询所有的key
+     * keys *name 查询所有以name结尾的key
+     * exists key 判断key是否存在
+     * del key 删除key 阻塞 也就是说会等待删除完成
+     * unlink key 删除key 但是不阻塞 也就是说不会等待删除完成
+     * type key 查询key的类型
+     * ttl key 查询key的过期时间 如果是-1 说明永不过期 如果是-2 说明已经过期
+     * expire key seconds 设置key的过期时间
+     * select index 选择数据库 默认是0-15
+     * move key dbindex 将key移动到指定的数据库
+     * flushdb 清空当前数据库
+     * flushall 清空所有数据库
+     * redis key是区分大小写的 命令不区分大小写
+     * help 查看帮助
+     */
 
 
     @Resource

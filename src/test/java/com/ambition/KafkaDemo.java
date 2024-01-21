3c96945020c597b9d52f991a420ebc5b51a50d0c
fuyingzhuang <fyz15731966959@163.com>
2024-01-21 19:10:31 +0800

    忽然对人生有了新的感悟！

--- src/test/java/com/ambition/KafkaDemo.java
+++ src/test/java/com/ambition/KafkaDemo.java
@@ -4,9 +4,10 @@ package com.ambition;
  * @author Ambition
  * @date 2024/1/19 17:29
  * KafkaDemo kafka学习
+ * 
  */
 public class KafkaDemo {
     public static void main(String[] args) {
-        
+
     }
 }

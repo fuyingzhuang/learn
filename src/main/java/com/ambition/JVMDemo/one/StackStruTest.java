104b83b64bb1faaa5ec1fff85de1eef0c6e905b6
fuyingzhuang <fyz15731966959@163.com>
2024-02-21 22:37:04 +0800

    Jvm学习

---
+++ src/main/java/com/ambition/JVMDemo/one/StackStruTest.java
@@ -0,0 +1,12 @@
+package com.ambition.JVMDemo.one;
+
+/**
+ * @author Ambition
+ * @date 2024/2/20 16:25
+ */
+public class StackStruTest {
+    public static void main(String[] args) {
+        int i = 2 + 3;
+    }
+}
+
